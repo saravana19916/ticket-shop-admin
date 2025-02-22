2efd309a042c0f032df6a016a94b8177d73356b6
Karthic <karthic.ganesan@ravenantech.com>
2025-02-23 01:46:09 +0530

    Video link added in material marketing

---
+++ src/components/VideoPlayer.tsx
@@ -0,0 +1,83 @@
+import React, { FC, Dispatch, SetStateAction } from "react";
+import ReactModal from 'react-modal';
+import ReactPlayer from "react-player";
+import { useThemeMode } from "../utils/useThemeMode";
+import { useWindowSize } from "react-use";
+import { XMarkIcon } from "@heroicons/react/16/solid";
+
+interface IVideoPlayerProps {
+  videoLink: string | undefined;
+  videoModalIsOpen: boolean;
+  setVideoLink: Dispatch<SetStateAction<string | undefined>>;
+  setVideoModalIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+
+const VideoPlayer: FC<IVideoPlayerProps> = ({ 
+  videoLink,
+  videoModalIsOpen,
+  setVideoLink,
+  setVideoModalIsOpen,
+}) => {
+  const { isDarkMode } = useThemeMode();
+  const windowWidth = useWindowSize().width;
+
+  const modalStyles = {
+    overlay: {
+      zIndex: "51"
+    },
+    content: {
+      zIndex: "52",
+      backgroundColor: "rgba(19,19,19,1)",
+      width: `${windowWidth > 600 ? "80%" : "100%"}`,
+      height: `${windowWidth > 600 ? "80%" : "50%"}`,
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      borderRadius: "0.75rem",
+      padding: "2rem 0 0 0",
+    }
+  };
+
+  const closeButtonStyles = {
+    position: "absolute",
+    top: "0.5rem",
+    right: "1rem",
+    color: "#FFF"
+  } as React.CSSProperties;
+
+  const handleCloseModal = (e: { stopPropagation: () => void; }) => {
+    e.stopPropagation();
+    setVideoLink(undefined);
+    setVideoModalIsOpen(false);
+  }
+
+  return (
+    <>
+      <div data-nc-id="VideoPlayer">
+        <ReactModal 
+          isOpen={videoModalIsOpen}
+          onRequestClose={handleCloseModal}
+          style={modalStyles}
+        >
+          {videoLink && (
+            <>
+              <button onClick={handleCloseModal} style={closeButtonStyles}>
+                <XMarkIcon className="w-6 h-6" aria-hidden="true" />
+              </button>
+              <ReactPlayer
+                url={videoLink}
+                height='100%'
+                width='100%'
+                controls={true}
+                className='w-full'
+              />
+            </>
+          )}
+        </ReactModal>
+      </div>
+    </>
+  )
+}
+
+export default VideoPlayer;
--- src/components/listing/AddListing/AddMaterialMarketing/AddVideoLink.tsx
+++ src/components/listing/AddListing/AddMaterialMarketing/AddVideoLink.tsx
@@ -7,8 +7,11 @@ import {
   FormInputStyled,
   FormLabelStyled,
 } from "../../../styledComponents/styledForm";
+import VideoPlayer from "../../../VideoPlayer";
+
 interface IProps {}
 const AddVideoLink: FC<IProps> = () => {
+  const [videoModalIsOpen, setVideoModalIsOpen] = useState<boolean>(false);
   const [videoLink, setVideoLink] = useState<undefined | string>();
   const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
 
@@ -24,6 +27,9 @@ const AddVideoLink: FC<IProps> = () => {
       toast.error("Please Insert Video Link!");
     }
   };
+  const handlePreview = () => {
+    setVideoModalIsOpen(true);
+  };
   return (
     <>
       <div className="d-flex gap-4 align-items-center flex-wrap">
@@ -38,11 +44,32 @@ const AddVideoLink: FC<IProps> = () => {
             />
           </Form.Group>
         </div>
-        <div className="mt-4">
-          <SubmitAndPreviewButtons
-            isSubmitted={isSubmitted}
-            handleSubmit={_handleSubmit}
-          />
+        <div
+          className="mt-4"
+          style={{
+            borderColor: "#dadbdf",
+            backgroundColor: "#e4e4e4",
+            borderRadius: "50px",
+            fontWeight: 500,
+            color: "#000",
+            padding: "10px 24px",
+          }}
+        >
+          <a href="#" onClick={handlePreview}>
+            <span
+              style={{
+                color: "#000",
+              }}
+            >
+              Preview
+            </span>
+            <VideoPlayer
+              videoLink={videoLink}
+              videoModalIsOpen={videoModalIsOpen}
+              setVideoLink={setVideoLink}
+              setVideoModalIsOpen={setVideoModalIsOpen}
+            />
+          </a>
         </div>
       </div>
     </>
